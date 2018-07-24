e6d65a46b48adba5333f511085840241aaabcfe8
Joshua Martin <joshua@bluefishds.com>
2018-07-24 10:11:47 -0400

    Add relational rest routes

--- api/rest.js
+++ api/rest.js
@@ -9,6 +9,24 @@ restRouter.get('/users/:id/blogs', (ctx) => {
   ctx.body = blogsByUser;
 });
 
+restRouter.get('/blogs/:id/author', (ctx) => {
+  let { id } = ctx.params;
+  let blog = blogs.find(blog => blog.id === id);
+  let { uid } = blog;
+  let authorFromBlog = users.find(user => user.id === uid);
+  ctx.body = authorFromBlog;
+});
+
+restRouter.get('/users/:id', (ctx) => {
+  let { id } = ctx.params;
+  ctx.body = users.find(user => user.id === id)
+});
+
+restRouter.get('/blogs/:id', (ctx) => {
+  let { id } = ctx.params;
+  ctx.body = blogs.find(blogs => blogs.id === id)
+});
+
 restRouter.get('/users', (ctx) => { ctx.body = users });
 restRouter.get('/blogs', (ctx) => { ctx.body = blogs });
 
